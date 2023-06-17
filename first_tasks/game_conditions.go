d7529bbd69cd73cdf97371ad9417576801fa7f32
lancegod <alexandr.levkovets@gmail.com>
2023-06-18 00:05:50 +0300

    completed new task of conditional operators

---
+++ first_tasks/game_conditions.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// Here I will compile this task
+// There are several locations in the adventure game in question.
+// Write a program that uses if and else if to
+// display a description of each of the three locations: caves, entrances, and mountains
+
+func displayDescription(location string) {
+	if location == "caves" {
+		fmt.Println("You don't see anything")
+	}
+	if location == "entrances" {
+		fmt.Println("You see dark cave")
+	}
+	if location == "mountains" {
+		fmt.Println("You see beautiful landscape")
+	}
+}
+
+func main() {
+	displayDescription("caves")
+	displayDescription("entrances")
+	displayDescription("mountains")
+}
